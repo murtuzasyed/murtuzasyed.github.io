45c4bd984d560946477233fb92cf2afe2744689c
Syed <murtuzasyed1987@gmail.com>
2020-03-06 02:33:13 -0600

    Chore: Updaed Readme with little description

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import Layout from "../../components/Layout"
-
+import "./index.scss"
 const contactOptions = [
   {
     label: "contact@murtuzasyed.com",
     onclick: ()=>window.location.href='mailto:contact@murtuzasyed.com',
-    href: "mailto:murtuzasyed1987@gmail.com",
+    href: "mailto:contact@murtuzasyed.com",
     path: "M12 .02c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.99 6.98l-6.99 5.666-6.991-5.666h13.981zm.01 10h-14v-8.485l7 5.673 7-5.672v8.484z"
   },
   {
